12d7df69fec2813233f3f71db84744966f3dc244
John <john@example.com>
2021-08-10 15:53:07 +0600

    complated

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,9 @@
 
-const Item = ({ work, completed,important,onClickDelete,onClickImportant }) => {
+const Item = ({onClickComplated, work, completed,important,onClickDelete,onClickImportant }) => {
 
     return (
         <li className={"list-group-item " + (completed ? "bg-success" : "")}>
-            <span className={important?"text-warning":""}>{work}</span>
+            <span type="button" onClick={()=>onClickComplated(work)} className={important?"text-warning":""}>{work}</span>
             <span onClick={()=>onClickDelete(work)} className="btn btn-danger float-end ms-2">x</span>
             <span onClick={()=>onClickImportant(work)} className="btn btn-warning float-end">!</span>
         </li>
--- src/components/ItemGroup.js
+++ src/components/ItemGroup.js
@@ -11,25 +11,40 @@ const ItemGroup = () => {
     ])
 
     const onClickDelete = (work) => {
-        const filterWork = items.filter((item)=>item.work !== work)
+        const filterWork = items.filter((item) => item.work !== work)
         setItems(filterWork)
     }
 
     const onClickImportant = (work) => {
-        const itemIndex = items.findIndex((item)=>item.work === work)
-        const currentItem = {...items[itemIndex]}
+        const itemIndex = items.findIndex((item) => item.work === work)
+        const currentItem = { ...items[itemIndex] }
         const newItems = [...items]
-        
+
         currentItem.important = !currentItem.important
         newItems[itemIndex] = currentItem
 
         setItems(newItems)
     }
 
+    const onClickComplated = (work) => {
+        const itemIndex = items.findIndex((item) => item.work === work)
+        const currentItem = { ...items[itemIndex] }
+        const newItems = [...items]
+        currentItem.completed = !currentItem.completed
+        newItems[itemIndex] = currentItem
+        setItems(newItems)
+    }
+
     return (
         <ul className="list-group">
             {items.map((item) => (
-                <Item onClickImportant={onClickImportant} onClickDelete={onClickDelete} work={item.work} important={item.important} completed={item.completed} />
+                <Item
+                    onClickComplated={onClickComplated}
+                    onClickImportant={onClickImportant}
+                    onClickDelete={onClickDelete}
+                    work={item.work}
+                    important={item.important}
+                    completed={item.completed} />
             ))}
         </ul>
     )
